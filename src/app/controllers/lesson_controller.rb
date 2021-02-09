e7c1fc49860e4f4a9cf4b2d77046421874c18f2e
ttk <ttk7813@gmail.com>
2021-02-09 22:28:49 +0900

    :rocket: Chapter3-3

--- src/app/controllers/lesson_controller.rb
+++ src/app/controllers/lesson_controller.rb
@@ -23,4 +23,42 @@ class LessonController < ApplicationController
   def step6
     render plain: flash[:notice]
   end
+
+  def step7
+    @price = (2000 * 1.08).floor
+  end
+
+  def step8
+    @price = 1000
+    render "step7"
+  end
+
+  def step9
+    @comment = "<script>alert('デデン！')</script>"
+  end
+
+  def step11
+    @population = 704414
+    @surface = 141.33
+  end
+
+  def step12
+    @time = Time.current
+  end
+
+  def step13
+    @population = 127767944
+  end
+
+  def step14
+    @message = "ごきげんいかが？¥nRailsの勉強をがんばりましょう。"
+  end
+
+  def step17
+    @zaiko = 10
+  end
+
+  def step18
+    @items = { "a" => 2680, "b" => 2550, "c" => 4515, "d" => 945 }
+  end
 end
